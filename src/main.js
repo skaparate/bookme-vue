b26157b19e6b53ba0015b0b864fdb27a9e8e028a
Nicolas Mancilla <nico@nicomv.com>
2019-11-27 16:39:56 -0300

    Added global styles and font awesome configuration

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import buefy from 'buefy';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import 'buefy/dist/buefy.css';
+import './assets/theme.sass';
 
 Vue.config.productionTip = false;
+library.add(fas);
 
 Vue.use(buefy);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   router,
